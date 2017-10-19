fc1ceef60d9915d0da6be9fabdfae8e32521b3af
daniel fernando betancur villada <danielpcpx@hotmail.com>
2017-10-19 14:07:46 -0500

    proyecto

---
+++ Polito/Polito/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Polito.MvcApplication" Language="C#" %>
---
+++ Polito/Polito/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Polito.Startup))]
+namespace Polito
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
